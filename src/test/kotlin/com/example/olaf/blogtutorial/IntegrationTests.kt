865bb76e1aedf965c42f4a2675dced0f195f720f
socar-olaf <olaf@socar.kr>
2020-03-19 14:02:40 +0900

    포맷에 맞게 코드 변경(추가)

--- src/test/kotlin/com/example/olaf/blogtutorial/IntegrationTests.kt
+++ src/test/kotlin/com/example/olaf/blogtutorial/IntegrationTests.kt
@@ -11,7 +11,7 @@ import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class IntegrationTests(@Autowired val restTemplate: TestRestTemplate){
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @BeforeAll
     fun setup() {
