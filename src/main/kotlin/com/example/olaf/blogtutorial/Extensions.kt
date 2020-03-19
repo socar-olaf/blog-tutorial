c378cfdb63e1db45c798bd4f9db5d69ccc3b2c2e
socar-olaf <olaf@socar.kr>
2020-03-19 10:39:35 +0900

    Add space befor bracket

--- src/main/kotlin/com/example/olaf/blogtutorial/Extensions.kt
+++ src/main/kotlin/com/example/olaf/blogtutorial/Extensions.kt
@@ -1,2 +1,24 @@
 package com.example.olaf.blogtutorial
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+
+
+fun LocalDateTime.format() = this.format(englishDateFormatter)
+
+private val daysLookup = (1..31).associate { it.toLong() to getOrdinal(it) }
+
+private val englishDateFormatter = DateTimeFormatterBuilder()
+    .appendPattern("yyyy-MM-dd")
+    .appendLiteral(" ")
+    .appendText(ChronoField.DAY_OF_MONTH, daysLookup)
+
+private fun getOrdinal(n: Int) = when {
+    n in 11..13 -> "${n}th"
+    n % 10 == 1 -> "${n}st"
+    n % 10 == 2 -> "${n}nd"
+    n % 10 == 3 -> "${n}rd"
+    else -> "${n}th"
+}
