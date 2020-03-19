32d802e54699be06c466134b8cf5952749c4bde9
socar-olaf <olaf@socar.kr>
2020-03-19 17:11:53 +0900

    Add test for article restTemplate entity

--- src/test/kotlin/com/example/olaf/blogtutorial/IntegrationTests.kt
+++ src/test/kotlin/com/example/olaf/blogtutorial/IntegrationTests.kt
@@ -20,15 +20,19 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `Assert blog page title, content and status code`() {
-        println(">> Assert blog page title")
+        println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "olaf")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val title = "when is complete tut"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Socar olaf", "tutorial is loooong")
     }
 
     @AfterAll
